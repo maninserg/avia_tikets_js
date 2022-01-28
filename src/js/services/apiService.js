fdb4a55bbd2c1b28f1091d80ea335492bbbffefd
maninserg <manin.serg@gmail.com>
2022-01-28 22:29:31 +0300

    Create methods of get data from server

---
+++ src/js/services/apiService.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import config from '../config/apiConfig';
+
+/**
+ * Endpoints of our server:
+ * '/countries' - get array of availible countries
+ * '/cities' - get array of availible cities
+ * 'prices/cheap' - get array availible flights'
+ */
+
+class Api {
+    constructor(config) {
+        this.url = config.url;
+    }
+    // method get all countries from server
+    async countries() {
+        try {
+            const response = await axios.get(`${this.url}/countries`);
+            return response.data;
+        } catch (err) {
+            console.log(err);
+            return Promise.reject(err);
+        }
+    }
+    // method get all cities from server
+    async cities() {
+        try {
+            const response = await axios.get(`${this.url}/cities`);
+            return response.data;
+        } catch (err) {
+            console.log(err);
+            return Promise.reject(err);
+        }
+    }
+    //method get all flights from server
+    async prices(params) { }
+}
+
+// Create instance of Api
+const api = new Api(config);
+
+export default api;
