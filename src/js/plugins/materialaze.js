33674ef599b1f20ea34e5cf21a1d366adcc31275
maninserg <manin.serg@gmail.com>
2022-01-29 23:56:31 +0300

    Add css classes like in bootstrap and stylization navbar

--- src/js/plugins/materialaze.js
+++ src/js/plugins/materialaze.js
@@ -4,3 +4,7 @@ import 'materialize-css/dist/js/materialize.min.js';
 // Init select
 const select = document.querySelectorAll('select');
 M.FormSelect.init(select);
+
+export function getSelectInstance(elem) {
+    return M.FormSelect.getInstance(elem);
+}
