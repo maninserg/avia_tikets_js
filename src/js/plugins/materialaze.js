a8b712bd2a8b0529a46711fe4600875d9e4da5e4
maninserg <manin.serg@gmail.com>
2022-01-29 23:24:58 +0300

    Add navbar and select currency

--- src/js/plugins/materialaze.js
+++ src/js/plugins/materialaze.js
@@ -1,2 +1,6 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min.js';
+
+// Init select
+const select = document.querySelectorAll('select');
+M.FormSelect.init(select);
