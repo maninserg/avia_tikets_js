0f3873bdf921bd9dd4fc2707dd9c5ac72da2b531
maninserg <manin.serg@gmail.com>
2022-01-30 17:39:17 +0300

    Add form for search flights

--- src/js/plugins/materialaze.js
+++ src/js/plugins/materialaze.js
@@ -1,10 +1,37 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min.js';
 
-// Init select
+
+// Init selects
 const select = document.querySelectorAll('select');
 M.FormSelect.init(select);
 
 export function getSelectInstance(elem) {
     return M.FormSelect.getInstance(elem);
+}
+
+
+// Init autocomplites
+const autocompletes = document.querySelectorAll('.autocomplete');
+M.Autocomplete.init(autocompletes, {
+    data: {
+        'Apple': null,
+        'Microsoft': null,
+        'Google': 'https://placehold.it/250x250',
+    },
+});
+
+export function getAutocompleteInstanse(elem) {
+    return M.Autocomplete.getInstance(elem);
+}
+
+
+// Init datepickers
+const datepickers = document.querySelectorAll('.datepicker');
+M.Datepicker.init(datepickers, {
+    showClearBtn: true,
+});
+
+export function getDatePickerInstance(elem) {
+    return M.Datepicker.getInstance(elem);
 }
