6f251a881968fd916d1fe6eb8fbf368a4a906b28
maninserg <manin.serg@gmail.com>
2022-02-01 20:34:28 +0300

    Add function for serialization of countries

--- src/js/store/locations.js
+++ src/js/store/locations.js
@@ -12,10 +12,19 @@ class Locations {
             this.api.cities()
         ]);
         const [counrties, cities] = response;
-        this.countries = counrties;
+        this.countries = this.serialazeCountries(counrties);
         this.cities = cities;
         return response;
     }
+    
+    serialazeCountries(countries) {
+        // {'Country code': {...}}
+        return countries.reduce((acc, country) => {
+            acc[country.code] = country;
+            return acc;
+        }, {});
+    }
+
     getCitiesByCountryCode(code) {
         return this.cities.filter(city => city.country_code === code);
     }
@@ -24,3 +33,4 @@ class Locations {
 const locations = new Locations(api);
 
 export default locations;
+
